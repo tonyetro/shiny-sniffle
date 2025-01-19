f00819354f491aeda93ade0bdf370549e8fcfbee
tonyetro <ssj.caelum@gmail.com>
2025-01-19 20:52:52 +1100

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_num_words(text):
 def get_chars_dict(text):
     chars = {}
     for c in text:
-        lowered = c.text()
+        lowered = c.lower()
         if lowered in chars:
             chars[lowered] += 1
         else:
