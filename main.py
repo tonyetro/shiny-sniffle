90309bda0133b015d55644476d4398a8f93e2f2e
tonyetro <ssj.caelum@gmail.com>
2025-01-19 20:51:48 +1100

    unique char count

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 def main():
-    with open("./frankenstein.txt") as f:
-        file_contents = f.read().split()
-        print(len(file_contents))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.text()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
