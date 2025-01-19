2fa6aea360de297a75e6dcad8c48616981ada4a7
tonyetro <ssj.caelum@gmail.com>
2025-01-19 19:12:09 +1100

    length of chars in book

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def main():
     with open("./frankenstein.txt") as f:
         file_contents = f.read().split()
-        # print(len(file_contents))
-        print("hello world")
+        print(len(file_contents))
 
 main()
