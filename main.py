a6b95da18eaa19cbb14642789a0df54d0c8f747b
tonyetro <ssj.caelum@gmail.com>
2025-01-19 20:54:34 +1100

    print result

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
+    print(chars_dict)
 
 def get_num_words(text):
     words = text.split()
